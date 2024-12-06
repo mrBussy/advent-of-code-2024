df9e60a1e11c1d43abd8be36de7239bc55f3f874
R. Middel <r.middel@mrbussy.eu>
2024-12-06 12:00:35 +0100

    Solved Day 3 - Part 1

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,7 +1,18 @@
+use regex::Regex;
+
 advent_of_code::solution!(3);
 
-pub fn part_one(_input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<u32> {
+    let regex = Regex::new(r"mul\((?P<x>[0-9]{1,3}),(?P<y>[0-9]{1,3})\)").unwrap();
+    Some(input.lines().map(|line|{
+        regex
+        .captures_iter(line)
+        .map(|cap| {
+            // within the match, find the group right and group left and extract the value
+            cap["x"].parse::<u32>().unwrap()
+            * cap["y"].parse::<u32>().unwrap()
+        }).sum::<u32>()
+    }).sum())
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
@@ -15,7 +26,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(161));
     }
 
     #[test]
