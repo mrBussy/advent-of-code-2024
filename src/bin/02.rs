6bc9ab067714120f8399c1c3be44c2263c152c3e
R. Middel <r.middel@mrbussy.eu>
2024-12-06 11:31:53 +0100

    Solved Day 2 - Part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -17,8 +17,30 @@ pub fn part_one(input: &str) -> Option<u32> {
     .count() as u32)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    Some(input.lines()
+
+    .filter_map(|line| { let split_line =line.split_ascii_whitespace()
+        .map(|item| item.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+        if split_line.windows(2).all(|w| matches!(w[1] - w[0], 1..=3)) ||
+           split_line.windows(2).all(|w| matches!(w[0] - w[1], 1..=3))
+            {Some(true)}
+        else
+            {
+                for i in 0..split_line.len() {
+                    let mut new_line = split_line.clone();
+                    new_line.remove(i);
+                    if new_line.windows(2).all(|w| matches!(w[1] - w[0], 1..=3)) ||
+                       new_line.windows(2).all(|w| matches!(w[0] - w[1], 1..=3))
+                        {return Some(true)}
+                }
+                None
+            }
+        }
+    )
+    .count() as u32)
 }
 
 #[cfg(test)]
@@ -34,6 +56,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
